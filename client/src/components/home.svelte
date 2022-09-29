e01241f591c9f1aa69d6b692ebe2276a6b501840
siddhantloya <siddhant@trell.in>
2022-09-29 21:04:19 +0530

    fix: make header common in layout (minor ui changes accordingly)

--- client/src/components/home.svelte
+++ client/src/components/home.svelte
@@ -44,19 +44,29 @@
 	}
 </script>
 
-<Header />
+<div class="header__tags no-scrollbar">
+	<!-- <p>Hello</p> -->
+	{#each Array(4) as _}
+		<button class="header__tags-btn">All</button>
+		<button class="header__tags-btn">Mixes</button>
+		<button class="header__tags-btn">Music</button>
+		<button class="header__tags-btn">Bollywood</button>
+		<button class="header__tags-btn">Street Food</button>
+	{/each}
+</div>
+<!-- <Header /> -->
 <Nav />
 
 <div class="feed">
 	{#each Array(feed.length) as _, i}
 		<VideoTile data={feed[i]} />
 	{/each}
-</div>
-<div class="continuation-row">
 	{#each Array(8) as _, i}
 		<VideoTileSkeleton />
 	{/each}
 </div>
+
+<div class="continuation-row" />
 
 <style>
 	.feed,
@@ -77,13 +87,57 @@
 		background: #f9f9f9;
 	}
 
+	.header__tags {
+		display: flex;
+		gap: 15px;
+		margin-left: 12vw;
+		overflow-x: scroll;
+		width: 88vw;
+		height: 60px;
+		align-items: center;
+
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+	}
+
+	.header__tags-btn {
+		background: rgba(0, 0, 0, 0.05);
+		padding: 5px 20px;
+		border-radius: 20px;
+		white-space: nowrap;
+		border: 1px solid rgba(0, 0, 0, 0.1);
+
+		font-size: 15px;
+	}
+
+	.header__tags-btn:first-child {
+		margin-left: 30px;
+	}
+
+	.header__tags-btn:last-child {
+		margin-right: 30px;
+	}
 	@media (max-width: 600px) {
-		.feed {
+		.feed,
+		.continuation-row {
 			left: 0px;
 			width: 100vw;
 			padding: 30px 0px;
 			grid-template-columns: repeat(1, 1fr);
 			row-gap: unset;
+		}
+
+		.header__tags {
+			width: 100vw;
+			margin-left: 0px;
+		}
+
+		.header__tags-btn:first-child {
+			margin-left: 10px;
+		}
+
+		.header__tags-btn:last-child {
+			margin-right: 10px;
 		}
 	}
 </style>
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
+	import Header from '@components/header.svelte';
 	import '../app.css';
 </script>
+
+<Header />
 
 <slot />
