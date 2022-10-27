bfb926cc95529751abea62e170411d79645e1da5
siddhantloya <siddhant@trell.in>
2022-10-27 15:06:48 +0530

    suggested videos UI, actions ui for mobile

--- client/src/components/watch/suggestions/videoSuggestions.svelte
+++ client/src/components/watch/suggestions/videoSuggestions.svelte
@@ -1,21 +1,70 @@
 <script>
 	import CategoryTag from '@components/common/categoryTag.svelte';
+	import MediaQuery from '@components/common/MediaQuery.svelte';
 </script>
 
-<div class="w-full ">
-	<div class="w-full flex gap-2 mb-2 overflow-x-scroll no-scrollbar">
-		<CategoryTag tagName="All" selected />
-		<CategoryTag tagName="Lorem" />
-		<CategoryTag tagName="Ipsum" />
-		<CategoryTag tagName="Dolor" />
-		<CategoryTag tagName="Sit" />
-		<CategoryTag tagName="Amet" />
-	</div>
+<MediaQuery query="(min-width: 480px)" let:matches>
+	{#if matches}
+		<!-- content here -->
+		<div class="w-full ">
+			<div class="w-full flex gap-2 mb-4 overflow-x-scroll no-scrollbar">
+				<CategoryTag tagName="All" selected />
+				<CategoryTag tagName="Lorem" />
+				<CategoryTag tagName="Ipsum" />
+				<CategoryTag tagName="Dolor" />
+				<CategoryTag tagName="Sit" />
+				<CategoryTag tagName="Amet" />
+			</div>
 
-	{#each new Array(12) as _}
-		<div class="bg-lime-300 h-32 mb-1 flex justify-center items-center	">Tile</div>
-	{/each}
-</div>
+			{#each new Array(12) as _}
+				<div class="mb-1 flex justify-center items-start mt-2">
+					<img class="rounded" src="https://picsum.photos/180/100" alt="hello" />
+					<div class="text-sm ml-2">
+						<p class="line-clamp-2 font-bold mb-1">
+							दादा गोवा छोड़ दो हमे | Sanjay, Arshad, Javed, Riteish, Aashish, Javed | Dhamaal Part
+							3
+						</p>
+						<p class="text-xs text-gray-600	mb-1">Shemaroo Comedy</p>
+						<p class="text-xs text-gray-600	">
+							<span>11M Views</span>
+							<span>&#183;</span>
+							<span>2 years ago</span>
+						</p>
+					</div>
+				</div>
+			{/each}
+		</div>
+	{/if}
+</MediaQuery>
+
+<MediaQuery query="(max-width: 481px)" let:matches>
+	{#if matches}
+		<p class="px-2 border-t-2 py-2 text-sm">Up Next</p>
+		<div class="w-full ">
+			{#each new Array(12) as _}
+				<div class="mb-1 flex flex-col  items-center">
+					<img class="w-full" src="https://picsum.photos/180/100" alt="hello" />
+					<div class="text-sm ml-2 mt-4 px-2">
+						<div class="flex gap-2">
+							<img class="w-12 h-12 rounded-full" src="https://picsum.photos/50/50" alt="hello" />
+							<div>
+								<p class="line-clamp-2 font-bold mb-1">
+									दादा गोवा छोड़ दो हमे | Sanjay, Arshad, Javed, Riteish, Aashish, Javed | Dhamaal
+									Part 3
+								</p>
+								<p class="text-xs text-gray-600	mb-1">
+									<span> Shemaroo Comedy</span>
+									<span>&#183;</span>
+									<span>11M Views</span>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	{/if}
+</MediaQuery>
 
 <style>
 </style>
--- client/src/components/watch/videoMeta/videoData.svelte
+++ client/src/components/watch/videoMeta/videoData.svelte
@@ -6,36 +6,53 @@
 <MediaQuery query="(max-width: 480px)" let:matches>
 	{#if matches}
 		<div>
-			<h1 class="px-2 pt-2 text-lg font-normal">
+			<h1 class="px-4 pt-2 text-lg font-normal">
 				Taarak Mehta Ka Ooltah Chashmah | तारक मेहता का उल्टा चश्मा | Episode 862 | 15th March, 2021
 			</h1>
 		</div>
-		<div class="px-2 flex text-xs">
+		<div class="px-4 flex text-xs">
 			<span>1.4M views</span>
 			<span class="px-1">&#183;</span>
 			<span>3 days ago</span>
 		</div>
-		<div class="w-full px-2 pt-2 flex justify-around">
-			<div class="flex justify-between items-center text-xs flex-col">
+		<div class="w-full px-4 mt-4 flex gap-2">
+			<img src="https://picsum.photos/35" alt="Logo" class="w-10 h-10 rounded-full" />
+			<div class="">
+				<p class="whitespace-nowrap text-sm">Rajshekhar Mamidana</p>
+				<p class="text-xs">282K subscribers</p>
+			</div>
+			<button
+				class="text-white bg-black px-4  ml-auto rounded-full flex items-center justify-between text-sm"
+				>Subscribe</button
+			>
+		</div>
+		<div class="flex gap-4 pl-4 mt-4 w-full overflow-scroll no-scrollbar">
+			<!-- <div class="flex gap-2 ml-auto 	"> -->
+			<div class="flex justify-center text-sm gap-2 bg-[#E8E8E8] px-8 py-2 rounded-full divide-x-2">
 				<Img className="w-6" src="icons/watch/like.svg" alt="Like" />
-				<p>84k</p>
-			</div>
-			<div class="flex justify-between items-center text-xs flex-col">
+				<p class="mr-4">84k</p>
+
 				<Img className="w-6" src="icons/watch/dislike.svg" alt="Like" />
-				<p>Dislike</p>
 			</div>
-			<div class="flex justify-between items-center text-xs flex-col">
+			<div
+				class="flex justify-center items-center text-sm gap-2 bg-[#E8E8E8] px-6 py-2 rounded-full"
+			>
 				<Img className="w-6" src="icons/watch/share.svg" alt="Like" />
 				<p>Share</p>
 			</div>
-			<div class="flex justify-between items-center text-xs flex-col">
-				<Img className="w-6" src="icons/watch/add-to-playlist-mobile.png" alt="Like" />
+			<div
+				class="flex justify-center items-center text-sm gap-2 bg-[#E8E8E8] px-6 py-2 rounded-full"
+			>
+				<Img className="w-6" src="icons/watch/add-to-playlist-desktop.svg" alt="Like" />
 				<p>Save</p>
 			</div>
-			<div class="flex justify-between items-center text-xs flex-col">
-				<Img className="w-6" src="icons/watch/report.svg" alt="Like" />
+			<div
+				class="flex justify-center items-center text-sm gap-2 bg-[#E8E8E8] px-6 py-2 rounded-full"
+			>
+				<Img className="w-4" src="icons/watch/report.svg" alt="Like" />
 				<p>Report</p>
 			</div>
+			<!-- </div> -->
 		</div>
 	{/if}
 </MediaQuery>
@@ -84,7 +101,7 @@
 				<div
 					class="flex justify-between items-center text-sm gap-2 bg-[#E8E8E8] px-4 py-2 rounded-full"
 				>
-					<Img className="w-6" src="icons/watch/report.svg" alt="Like" />
+					<Img className="w-4" src="icons/watch/report.svg" alt="Like" />
 					<p>Report</p>
 				</div>
 			</div>
